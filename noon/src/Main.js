cc47ff42b997ce124dea0591413f5ec30f57ffca
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-07-10 22:14:16 +1000

    initial Set up

---
+++ noon/src/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+               <h1> "Noon Report" </h1> 
+            </div>
+            
+        )
+    }
+}
+
+export default Main 
