3c50a8f56b5aa7cdbcf184c5718dbdbfc5333a1e
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-07-16 18:11:12 +1000

    Adding Elements

--- noon/src/Main.js
+++ noon/src/Main.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Header  from './Header';
 import { Component } from "react";
+import InputExampleInput from "./InputExampleinput";
+import GridExampleColumnWidth from './VesselInfo'
+import { Grid } from "semantic-ui-react";
+
 
 class Main extends Component{
     render(){
         return(
             <div>
-               <h1> "Noon Report" </h1> 
+               
+               <Header/>
+               <div class="ui action input"><input type="text" placeholder="Vessel name..."/><button class="ui button">Search</button></div>
+                <GridExampleColumnWidth/>
             </div>
             
         )
