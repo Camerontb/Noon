8d73d26fd2a230a501c45941aec8e68bf1efd12d
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-07-17 22:27:19 +1000

    Database consnole logging

--- Server/Server.js
+++ Server/Server.js
@@ -1,2 +1,29 @@
 const express = require ('express')
 const cors = require ('cors')
+
+const bodyParser = require ('body-parser')
+const connection = require ('./db')
+
+
+const app = express ()
+app.use(cors())
+app.use(bodyParser.json())
+
+
+//Program 
+
+app.get('/test',(req,res)=>{
+    let sql = ` SELECT * FROM Vessel `
+    connection.query(sql,(err,result, fields)=>{
+        if(err)throw err
+       console.log(result)
+        res.send(result)
+  
+    })
+  
+  })
+
+
+app.listen(4000,()=>{
+    console.log("Running on Port 4000")
+})
--- Server/db.js
+++ Server/db.js
@@ -1,11 +1,11 @@
 const mysql = require ('mysql2')
 
 const connection =mysql.createConnection({
-    host:'localhost',
-    user: 'root'
-    password:'root'
-    database: 'Vessel'
-    port: 8889
+    host: "localhost",
+    user: "root",
+    password:'root',
+    database: "Ships",
+    port:8889
 })
 
 module.exports = connection
