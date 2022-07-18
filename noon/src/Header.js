b796f2c42179b3b7ce3e7edf24914d478a421cc6
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-07-18 21:29:15 +1000

    Axios installed (Needs code work)

--- noon/src/Header.js
+++ noon/src/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from 'semantic-ui-react'
+import axios from 'axios'
+
+axios.get('http://localhost:4000/test')
+  .then(function (res, data) {
+    // handle success
+    console.log(res.data);
+    console.log(data.Name)
+    
+  },[])
 
 const HeaderExamplePage = () => (
   <div>
-    <Header as='h1'>Noon Report</Header>
+    <Header as='h1'>Sea-Swift Noon Report</Header>
     {/* <Header as='h2'>Second Header</Header>
     <Header as='h3'>Third Header</Header>
     <Header as='h4'>Fourth Header</Header>
