122ef1cd44d5daed17c7e2c7c85a23d01131cc88
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-09-13 22:13:07 +1000

    Browser Router working

--- noon/src/Main.js
+++ noon/src/Main.js
@@ -3,35 +3,36 @@ import Header  from './Header';
 import { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import './App.css';
-
+// import react
 
 import FormExampleWidthField from "./FormExampleWidthField";
 import MenuExampleSecondary from "./Menu"
 import Footer from "./Footer";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 class Main extends Component{
     render(){
         return(
-            <div>
-               
-               <Header/>
-               <MenuExampleSecondary />
-
-               {/* <div className="wrapper" class="ui action input"><input type="text" placeholder="Vessel name..."/><button class="ui button">Search</button></div> */}
-               
-               
+            <BrowserRouter>
+                <Routes>
                 
-                {/* <GridExampleColumnWidth/> */}
-                {/* <Form/> */}
-                <FormExampleWidthField/>
-                {/* <Footer classname = "wrapper"/> */}
+                {/* <Header/> */}
+                {/* <MenuExampleSecondary /> */}
 
+                {/* <div className="wrapper" class="ui action input"><input type="text" placeholder="Vessel name..."/><button class="ui button">Search</button></div> */}
                 
-            </div>
-            
-        )
+                
+                    
+                    {/* <GridExampleColumnWidth/> */}
+                    {/* <Form/> */}
+                    <Route path ='/' element= {<FormExampleWidthField/>}/>
+                    {/* <Footer classname = "wrapper"/> */}
+
+                </Routes>   
+            </BrowserRouter>   
+        )   
+        
     }
 }
 
